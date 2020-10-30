78b3f4c26cd9ead300d4a7589cacd9281e97d465
nonoll <hyunkwan.roh@nhntoast.com>
2020-10-30 09:30:55 +0900

    feat: tdd - chapter1, 16진행

---
+++ 04-tdd/src/chapter-1/16.ts
@@ -0,0 +1,118 @@
+export interface Expression {
+  times(multiplier: number): Expression;
+  plus(addend: Expression): Expression;
+  reduce(bank: Bank, to: string): Money;
+}
+
+export class Sum implements Expression {
+  constructor(public augend: Expression, public addend: Expression) {}
+
+  times(multiplier: number): Expression {
+    return new Sum(this.augend.times(multiplier), this.addend.times(multiplier));
+  }
+
+  plus(addend: Expression): Expression {
+    return new Sum(this, addend);
+  }
+
+  reduce(bank: Bank, to: string): Money {
+    // const amount = this.augend.amount + this.addend.amount;
+    // return new Money(amount, to);
+    const amount = this.augend.reduce(bank, to).amount + this.addend.reduce(bank, to).amount;
+    return new Money(amount, to);
+  }
+}
+
+export class Pair {
+  constructor(private from: string, private to: string) {}
+
+  key() {
+    return `${this.from}_${this.to}`;
+  }
+
+  equals(object: Object) {
+    const pair = object as Pair;
+    return this.from === pair.from && this.to === pair.to;
+  }
+
+  hashCode() {
+    return 0;
+  }
+}
+
+export class Bank {
+  private rates = new Map<string, number>();
+
+  addRate(from: string, to: string, rate: number) {
+    this.rates.set(new Pair(from, to).key(), rate);
+  }
+
+  rate(from: string, to: string) {
+    if (from === to) {
+      return 1;
+    }
+
+    const rate = this.rates.get(new Pair(from, to).key());
+
+    return rate || 1;
+    // return from === 'CHF' && to === 'USD' ? 2 : 1;
+  }
+
+  reduce(source: Expression, to: string): Money {
+    // if (source instanceof Money) {
+    //   return source;
+    // }
+    // return (source as Sum).reduce(to);
+    return source.reduce(this, to);
+  }
+}
+
+export class Money implements Expression {
+  constructor(public amount: number, protected _currency: string) {}
+
+  static dollar(amount: number): Money {
+    return new Money(amount, 'USD');
+  }
+
+  static franc(amount: number): Money {
+    return new Money(amount, 'CHF');
+  }
+
+  times(multiplier: number): Expression {
+    return new Money(this.amount * multiplier, this._currency);;
+  };
+
+  equals(money: Money): boolean {
+    return this.amount === money.amount && this.currency() === money.currency();
+  }
+
+  currency(): string {
+    return this._currency;
+  }
+
+  toString(): string {
+    return `${this.amount}  ${this._currency}`;
+  }
+
+  plus(addend: Expression): Expression {
+    // return new Money(this.amount + addend.amount, this._currency);
+    return new Sum(this, addend);
+  }
+
+  reduce(bank: Bank, to: string): Money {
+    const rate = bank.rate(this.currency(), to);
+    return new Money(this.amount / rate, to);
+  }
+}
+
+export class Dollar extends Money {
+  constructor(amount: number, _currency: string = 'USD') {
+    super(amount, _currency);
+  }
+}
+
+export class Franc extends Money {
+  constructor(amount: number, _currency: string = 'CHF') {
+    super(amount, _currency);
+  }
+}
