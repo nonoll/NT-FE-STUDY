87fa2cec956fbb96c5a156bce5e06f9244cfed13
nonoll <hyunkwan.roh@nhntoast.com>
2020-11-03 20:02:37 +0900

    [feature/js-env] fix: pr 리뷰 반영
    - PROJECT_PATH : process.cwd() 로 변경 
    - npm run start : open 옵션 제거, webpack.config.dev-server 에서 처리
    - npm run prebuild : del:dist 제거, clean-webpack-plugin 적용 
    - webpackConfigPlugins : 개발 / 배포 모드 분기 사항 고려

--- 05-javascript-dev-env/build/common.js
+++ 05-javascript-dev-env/build/common.js
@@ -1,6 +1,6 @@
 import * as path from 'path';
 
-export const PROJECT_PATH = path.dirname('../');
+export const PROJECT_PATH = process.cwd();
 
 export const resolvePath = relPath => path.resolve(PROJECT_PATH, relPath);
 export const relativePath = relPath => path.relative(PROJECT_PATH, relPath);
@@ -8,3 +8,13 @@ export const joinPath = relPath => path.join(PROJECT_PATH, relPath);
 
 export const isDevMode = args => args.mode === 'development';
 export const nameByENV = (IS_DEV_MODE, ext = '.[ext]') => IS_DEV_MODE ? `[name]${ext}` : `[name].[hash]${ext}`;
+
+export const useOnlyDevMode = (plugin, args) => {
+  // eslint-disable-next-line no-console
+  console.log('isDevMode(args)', isDevMode(args));
+  if (!isDevMode(args)) {
+    return null;
+  }
+
+  return plugin;
+};
--- 05-javascript-dev-env/build/webpack.config.plugins.js
+++ 05-javascript-dev-env/build/webpack.config.plugins.js
@@ -1,29 +1,27 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
-import {isDevMode, resolvePath} from './common';
+import {CleanWebpackPlugin} from 'clean-webpack-plugin';
+import {resolvePath, useOnlyDevMode} from './common';
 
-export const webpackConfigPlugins = (_, args, packageJson) => {
-  const IS_DEV_MODE = isDevMode(args);
-
-  return [
-    new HtmlWebpackPlugin({
-      title: packageJson.name,
-      template: resolvePath('public/index.html'),
-      filename: 'index.html',
-      chunks: [
-        'bundle'
-      ]
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: IS_DEV_MODE ? 'server' : 'static',
-      analyzerHost: 'localhost',
-      analyzerPort: 'auto',
-      reportFilename: 'BundleAnalyzerReport.html',
-      openAnalyzer: IS_DEV_MODE,
-      excludeAssets: ['node_modules'],
-      statsOptions: {
-        exclude: /node_modules/
-      }
-    })
-  ];
-};
+export const webpackConfigPlugins = (_, args, packageJson) => [
+  new CleanWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    title: packageJson.name,
+    template: resolvePath('public/index.html'),
+    filename: 'index.html',
+    chunks: [
+      'bundle'
+    ]
+  }),
+  useOnlyDevMode(new BundleAnalyzerPlugin({
+    analyzerMode: 'server',
+    analyzerHost: 'localhost',
+    analyzerPort: 'auto',
+    reportFilename: 'BundleAnalyzerReport.html',
+    openAnalyzer: true,
+    excludeAssets: ['node_modules'],
+    statsOptions: {
+      exclude: /node_modules/
+    }
+  }), args)
+].filter(plugin => plugin);
